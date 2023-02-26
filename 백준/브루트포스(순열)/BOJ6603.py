03206ee96f154f5dbaa47aad78e5890d7232f4ad
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2023-02-27 00:07:36 +0900

    2023.02.27

--- 백준/브루트포스(순열)/BOJ6603.py
+++ 백준/브루트포스(순열)/BOJ6603.py
@@ -1,26 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-w = [list(map(int, input().split())) for _ in range(n)]
-min_value = 4000000
 
-s = []
-
-
-def dfs(next, value, visited):
-    global min_value
-
-    if len(visited) == n:
-        if w[next][0] != 0:
-            min_value = min(min_value, value + w[next][0])
+def dfs(start, depth):
+    if depth == 6:
+        print(*res)
         return
-    for i in range(n):
-        if w[next][i] != 0 and i not in visited and value < min_value:
-            visited.append(i)
-            dfs(i, value + w[next][i], visited)
-            visited.pop()
+    for i in range(start, len(s)):
+        res.append(s[i])
+        dfs(i + 1, depth + 1)
+        res.pop()
 
 
-dfs(0, 0, [0])
-print(min_value)
+while True:
+    s = list(map(int, input().split()))
+    if s[0] == 0:
+        break
+    res = []
+    dfs(1, 0)
+    print()
